3df15904679ff629c71c6ba8a0f5b24189c72b57
Praisecodes <cmancoder@gmail.com>
2022-08-31 14:33:29 +0100

    complete

--- main.go
+++ main.go
@@ -4,6 +4,22 @@ import (
 	"fmt"
 )
 
+func Copy(firstArr [15]int, secondArr [15]int) {
+	firstArrLength := len(firstArr)
+	AddedCount := 0
+
+	for i := 0; i < firstArrLength; i++ {
+		secondArr[i] = firstArr[i]
+		AddedCount++
+
+		percent := (100 * AddedCount) / firstArrLength
+		fmt.Println(percent)
+	}
+}
+
 func main() {
-	fmt.Println("Hello")
+	var firstArray = [15]int{34, 89, 39, 58, 40, 58, 93, 34, 58, 40, 58, 39, 58, 35, 58}
+	var secondArray [15]int
+
+	Copy(firstArray, secondArray)
 }
